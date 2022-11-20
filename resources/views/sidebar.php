2b8c4f9ff26dee254c3bea26d12b11c199af6515
pasindu-dil <dilshanpasindu357@gmail.com>
2022-11-20 08:42:14 +0530

    Rename - use case diagram

--- resources/views/sidebar.php
+++ resources/views/sidebar.php
@@ -86,9 +86,4 @@
         </li>
       </ul>
     </div>
-    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-      <div class="mx-3">
-        <a class="btn bg-gradient-primary mt-4 w-100" href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
-      </div>
-    </div>
   </aside>
