d34893cd25a7603f3cac316427b3383a339874ac
pasindu-dil <dilshanpasindu357@gmail.com>
2022-12-04 14:30:41 +0530

    Add - jquery form validations

---
+++ resources/views/forms/productForm.php
@@ -0,0 +1,59 @@
+<div class="modal-body">
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productName">Name</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control is-invalid" name="name" id="productName" placeholder="Name">
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productItemCode">Item Code</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control is-invalid" name="itemCode" id="productItemCode" placeholder="Item code">
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productSellingPrice">Selling Price</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control is-invalid" name="sellingPrice" id="productSellingPrice" placeholder="Selling price">
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productQuantity">Quantity</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control is-invalid" name="quantity" id="productQuantity" placeholder="Quantity">
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productCategory">Category</label>
+        <div class="col-sm-8">
+            <select class="form-select form-control is-invalid" name="category" id="productCategory" aria-label="Default select example" aria-placeholder="Select category">
+                <option selected disabled>Select category</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productSubCategory">Sub Category</label>
+        <div class="col-sm-8">
+            <select class="form-select form-control is-valid" name="subCategory" id="productSubCategory" aria-label="Default select example" aria-placeholder="Sub category">
+                <option selected disabled>Select category</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productRemarks">Remarks</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control is-invalid" name="remarks" id="productRemarks" placeholder="Remarks">
+        </div>
+    </div>
+    <div class="form-group row mb-2">
+        <label class="col-sm-3 col-form-label" for="productDescription">Description</label>
+        <div class="col-sm-8">
+            <textarea class="form-control is-valid" name="description" placeholder="Leave a description here" id="productDescription"></textarea>
+        </div>
+    </div>
+</div>
